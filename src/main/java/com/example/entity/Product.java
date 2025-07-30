f570b861fd72abafb4ae2dd82e6ae7dcf49ff655
AlaaYehia <alasyehia5@gmail.com>
2025-07-31 02:46:54 +0300

    changes

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -1,4 +1,12 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
