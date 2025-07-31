d3b5afe464ec97e5f7541fd7531c2aa1261ced41
AlaaYehia <alasyehia5@gmail.com>
2025-07-31 03:34:35 +0300

    add new data

--- src/main/java/com/example/entity/Product.java
+++ src/main/java/com/example/entity/Product.java
@@ -1,11 +1,15 @@
 package com.example.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @Table
+@Setter
+@Getter
 public class Product {
 
     @Id
@@ -15,4 +19,6 @@ public class Product {
     private String name;
 
     private Date date;
+
+
 }
